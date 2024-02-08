ff3e8dec4b38cfe4cb3bfaae049862e72a0f825a
NickJeff13 <nick.w.jeffery3@gmail.com>
2024-02-08 09:55:44 -0400

    Update CBP_Transcriptomes.sh

--- code/CBP_Transcriptomes.sh
+++ code/CBP_Transcriptomes.sh
@@ -49,14 +49,52 @@ bowtie2 -p 40 -q --no-unal -k 20 -x SurfClam.Trinity.fasta \
   samtools sort SurfClamTransc.bam -o SurfClamTrans.coordSorted.bam
   samtools index SurfClamTrans.coordSorted.bam
   samtools faidx SurfClam.Trinity.fasta
-  igv.sh -g SurfClam.Trinity.fasta SurfClamTrans.coordSorted.bam
+  igv -g SurfClam.Trinity.fasta SurfClamTrans.coordSorted.bam
   
 #estimating transcript abundance with perl scripts
   sudo docker run -v`pwd`:`pwd` trinityrnaseq/trinityrnaseq \
   /usr/local/bin/trinityrnaseq/util/align_and_estimate_abundance.pl
       
   #get N50 values etc 
-  $TRINITY_HOME/util/TrinityStats.pl  Trinity.fasta
+  mnt/sdb/Trinity/util/TrinityStats.pl  SurfClam.Trinity.fasta
+
+'  
+################################
+## Counts of transcripts, etc.
+################################
+Total trinity 'genes':	248205
+Total trinity transcripts:	369395
+Percent GC: 36.42
+
+########################################
+Stats based on ALL transcript contigs:
+########################################
+
+	Contig N10: 6590
+	Contig N20: 4370
+	Contig N30: 3101
+	Contig N40: 2215
+	Contig N50: 1539
+
+	Median contig length: 414
+	Average contig: 840.70
+	Total assembled bases: 310550820
+
+
+#####################################################
+## Stats based on ONLY LONGEST ISOFORM per 'GENE':
+#####################################################
+
+	Contig N10: 5254
+	Contig N20: 3157
+	Contig N30: 2016
+	Contig N40: 1303
+	Contig N50: 877
+
+	Median contig length: 369
+	Average contig: 647.23
+	Total assembled bases: 160645243
+'
   
  #2. Use BUSCO to assess completeness
  #Created a conda environment called SeaLemonBusco when installing newest version of Busco
@@ -64,3 +102,16 @@ bowtie2 -p 40 -q --no-unal -k 20 -x SurfClam.Trinity.fasta \
  busco -i SurfClam.Trinity.fasta -l metazoa_odb10 -o SurfClamTranscBUSCOout --mode transcriptome
  
 conda deactivate
+'
+--------------------------------------------------
+	|Results from dataset mollusca_odb10              |
+	--------------------------------------------------
+	|C:81.6%[S:33.0%,D:48.6%],F:3.1%,M:15.3%,n:5295   |
+	|4318	Complete BUSCOs (C)                       |
+	|1745	Complete and single-copy BUSCOs (S)       |
+	|2573	Complete and duplicated BUSCOs (D)        |
+	|165	Fragmented BUSCOs (F)                     |
+	|812	Missing BUSCOs (M)                        |
+	|5295	Total BUSCO groups searched               |
+	--------------------------------------------------
+'
